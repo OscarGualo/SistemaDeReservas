a9615c4895a46ca946fba212e9cb6936556c69a1
Oscar <oscar.gualotuna@epn.edu.ec>
2025-11-07 13:58:52 -0500

    Se agrega la relacion entre paquete y servicios 1 a n, relacion entre promocion 1 a n, y la realcion entre paquete y habitacion 1 a n

--- SistemaDeVentas/src/main/java/org/example/Paquete.java
+++ SistemaDeVentas/src/main/java/org/example/Paquete.java
@@ -1,12 +1,22 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Paquete {
     private String nombre;
     private String descripcion;
     private double precioBase;
-    private List<Servicio> serviciosIncluidos;
-    private List<Promocion> promocionesAplicadas;
-    private List<Habitacion> habitaciones;
+    private List<Servicio> serviciosIncluidos; //REALCION ENTRE PAQUETE Y SERVICIO 1 A N
+    private List<Promocion> promocionesAplicadas; //RELACION ENTRE PAQUETE Y PROMOCION 1 A N ACA SE INCLUYE COMPOSICION ENVEZ DE AGREGACION COMO ESTA EN EL DIAGGRAMA
+    private List<Habitacion> habitaciones; //RELACION ENTRE PAQUETE Y HABITACION 1 A N
+
+    public Paquete(String nombre, String descripcion, double precioBase) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precioBase = precioBase;
+        this.serviciosIncluidos = new ArrayList<>();
+        this.promocionesAplicadas = new ArrayList<>();
+        this.habitaciones = new ArrayList<>();
+    }
 }
