e37e4fa58c14488c115762f16e0cd3d05548050c
Oscar <oscar.gualotuna@epn.edu.ec>
2025-11-07 14:35:12 -0500

    Se agrega los metodos abstractos en recepcionista y en reserva se define sus relacioens

--- SistemaDeVentas/src/main/java/org/example/Recepcionista.java
+++ SistemaDeVentas/src/main/java/org/example/Recepcionista.java
@@ -6,4 +6,19 @@ public class Recepcionista extends Usuario{
     public Recepcionista(String cedula, String nombres, String apellidos, String email, String contrasena) {
         super(cedula, nombres, apellidos, email, contrasena);
     }
+
+    @Override
+    public void iniciarSesion() {
+        System.out.println("Iniciando Sesion cliente-..... ");
+    }
+
+    @Override
+    public void cerrarSesion() {
+        System.out.println("Cerrando Sesion cliente-..... ");
+    }
+
+    @Override
+    public void modificarDatos() {
+        System.out.println("Modificando Sesion cliente-..... ");
+    }
 }
--- SistemaDeVentas/src/main/java/org/example/Reserva.java
+++ SistemaDeVentas/src/main/java/org/example/Reserva.java
@@ -8,9 +8,17 @@ public class Reserva {
     private LocalDate fechaEntrada;
     private LocalDate fechaSalida;
     private EstadoReserva estado;
-    private List<Habitacion> habitaciones;
-    private Paquete paquete;
-    private Cliente cliemte;
+    private List<Habitacion> habitaciones; // relacion de agreagacion 1 a 1 o 1 a n
+    private Paquete paquete; // Relacion de agregacion 1 a n
+    private Cliente cliente; // Relacion de asociacion 1 a n
 
-
+    public Reserva(String codigoReserva, LocalDate fechaEntrada, LocalDate fechaSalida, EstadoReserva estado, List<Habitacion> habitaciones, Paquete paquete) {
+        this.codigoReserva = codigoReserva;
+        this.fechaEntrada = fechaEntrada;
+        this.fechaSalida = fechaSalida;
+        this.estado = estado;
+        this.habitaciones = habitaciones;
+        this.paquete = paquete;
+        this.cliente = cliente;
+    }
 }
