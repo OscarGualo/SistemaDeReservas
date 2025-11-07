d36692a02316fdf2f149d0cbfb5af449bdc7fcd0
Oscar <oscar.gualotuna@epn.edu.ec>
2025-11-07 13:56:09 -0500

    Se agrega los metodos sobrescritos en cliente y la relacion bidireccional con la clase reserva

--- SistemaDeVentas/src/main/java/org/example/Cliente.java
+++ SistemaDeVentas/src/main/java/org/example/Cliente.java
@@ -1,10 +1,29 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cliente extends Usuario{
     private String direccion;
     private String telefono;
+    private List<Reserva> reservas = new ArrayList<>(); //RELACION BIDIRECCIONAL * CON LA CLASE RESERVA
 
     public Cliente(String cedula, String nombres, String apellidos, String email, String contrasena) {
         super(cedula, nombres, apellidos, email, contrasena);
     }
+    @Override
+    public void iniciarSesion(){
+        System.out.println("Iniciando Sesion cliente-..... ");
+    }
+
+    @Override
+    public void cerrarSesion() {
+        System.out.println("Cerrando Sesion cliente-..... ");
+    }
+
+    @Override
+    public void modificarDatos() {
+        System.out.println("Modificando Sesion cliente-..... ");
+    }
+
 }
