9c4871a03ab01c9c33afd5080ec28ba648f59f4a
Oscar <oscar.gualotuna@epn.edu.ec>
2025-11-07 13:55:27 -0500

    Se agrega los metodos sobrescritos en admin

--- SistemaDeVentas/src/main/java/org/example/Administrador.java
+++ SistemaDeVentas/src/main/java/org/example/Administrador.java
@@ -5,4 +5,19 @@ public class Administrador extends Usuario{
     public Administrador(String cedula, String nombres, String apellidos, String email, String contrasena) {
         super(cedula, nombres, apellidos, email, contrasena);
     }
+    @Override
+    public void iniciarSesion(){
+        System.out.println("Iniciando Sesion administrador.....");
+    }
+
+    @Override
+    public void cerrarSesion() {
+        System.out.println("Cerrando Sesion cliente-..... ");
+    }
+
+    @Override
+    public void modificarDatos() {
+        System.out.println("Modificando Sesion cliente-..... ");
+
+    }
 }
