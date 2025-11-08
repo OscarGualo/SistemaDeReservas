7544dd0f4ac353cee2efea14cd7e39ebb35fd7ad
SabinaZabala <zabalasabina14@gmail.com>
2025-11-08 00:10:31 -0500

    Clase Cliente con metodos y atributos correctos

--- SistemaDeVentas/src/main/java/org/example/Cliente.java
+++ SistemaDeVentas/src/main/java/org/example/Cliente.java
@@ -1,10 +1,25 @@
 package org.example;
 
-public class Cliente extends Usuario{
+public class Cliente extends Usuario {
     private String direccion;
-    private String telefono;
+    private String preferencias;
 
-    public Cliente(String cedula, String nombres, String apellidos, String email, String contrasena) {
+    public Cliente(String cedula, String nombres, String apellidos, String email, String contrasena, String direccion, String preferencias) {
         super(cedula, nombres, apellidos, email, contrasena);
+        this.direccion = direccion;
+        this.preferencias = preferencias;
+    }
+
+    public void realizarReserva() {
+        // Lógica para realizar una reserva
+    }
+    public void cancelarReserva() {
+        // Lógica para cancelar una reserva
+    }
+    public void verReservas() {
+        // Lógica para ver reservas
+    }
+    public void modificarReserva() {
+        // Lógica para modificar una reserva
     }
 }
